6004bfeaee5861c89f04e000d28a17fd9d4086d4
shivaramakrishna906 <shivaramakrishna906@gmail.com>
2025-08-30 11:55:11 +0530

    Add JPA entity creation guide and implement Student entity class

---
+++ spring-data-jpa-tutorial/src/main/java/com/krishnaallu009/spring_data_jpa_tutorial/entity/Student.java
@@ -0,0 +1,27 @@
+package com.krishnaallu009.spring_data_jpa_tutorial.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
