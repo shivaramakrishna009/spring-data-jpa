33a1d8119da4357ff2e7655bdcb4df98d0db8686
shivaramakrishna906 <shivaramakrishna906@gmail.com>
2025-08-30 10:50:22 +0530

    initialize Spring Data JPA tutorial project with Maven setup and basic application structure

---
+++ spring-data-jpa-tutorial/src/test/java/com/krishnaallu009/spring_data_jpa_tutorial/SpringDataJpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishnaallu009.spring_data_jpa_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
